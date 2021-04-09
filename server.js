fc7e8a6c53f79e159fbdfce7113c924ba3a90033
Daniel Hros <danielhros33@gmail.com>
2021-04-09 20:52:44 +0200

    develop and deploy differencing

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
