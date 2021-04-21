b7e31d5b99e72c171fe34b95ac0a0ef849a502ed
Daniel Hros <danielhros33@gmail.com>
2021-04-21 23:21:53 +0200

    gateway config table populate

--- db/populate.js
+++ db/populate.js
@@ -10,6 +10,7 @@ const populateDatabase = async () => {
   // await setRandomDutyCycleRemainingDownlink();
   // await setRandomDutyCycleRemainingUplink();
   // await addGeoToNodes();
+  // await addApsConfig();
 };
 
 // min and max included
@@ -156,6 +157,32 @@ const addGeoToNodes = async () => {
         `WHERE dev_id = ${i}`
     );
     console.log(i);
+  }
+};
+
+const addApsConfig = async () => {
+  let i = 0;
+
+  while (true) {
+    let query = {
+      text: `select id from aps limit 1 offset ${i}`,
+    };
+
+    let { rows } = await db.query(query.text);
+    let apId = rows[0]?.id;
+
+    if (apId == undefined) {
+      break;
+    }
+
+    query.text =
+      `insert into aps_config (setap, gateway_id) ` +
+      `VALUES ('{"message_name":"SETAP","ap_id":"${apId}","message_body":[{"type":"NORMAL","cr":"4/5","freqs":[867.1,"none","none","none","none","none","none","none","none"],"band":125,"power":5,"sf":7},{"type":"EMER","cr":"4/5","freqs":["none","none","none","none","none",868.3,"none","none","none"],"band":125,"power":5,"sf":7},{"type":"REG","cr":"4/5","freqs":[867.3,"none",867.5,"none","none","none","none","none","none"],"band":125,"power":5,"sf":7}]}'::json, '${apId}')`;
+
+    await db.query(query.text);
+
+    console.log(i);
+    i += 1;
   }
 };
 
